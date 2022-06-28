6e0b466051c0a6a48d75107c21c2ab59468b312a
Fossette <3373611620@qq.com>
2022-06-28 10:34:39 +0800

    feat(element-plus $ login页面布局): 安装element-plus & svg封装和使用 & login页面布局

--- src/main.js
+++ src/main.js
@@ -2,5 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import './style/index.scss'
+import installIcons from './icons'
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App)
+installIcons(app)
+
+app.use(store).use(router).use(ElementPlus).mount('#app')
---
+++ src/views/login/index.vue
@@ -0,0 +1,144 @@
+<template>
+  <div class="login-container">
+    <el-form class="login-form" :model="loginForm">
+      <div class="title-container">
+        <h3 class="title">用户登录</h3>
+        <svg-icon className="svg-language" icon="language"></svg-icon>
+      </div>
+      <!-- 用户名 -->
+      <el-form-item prop="username">
+        <span class="svg-container">
+          <el-icon>
+            <svg-icon icon="user"></svg-icon>
+          </el-icon>
+        </span>
+        <el-input
+          placeholder="username"
+          name="username"
+          type="text"
+          v-model="loginForm.username"
+        ></el-input>
+      </el-form-item>
+      <!-- 密码框 -->
+      <el-form-item prop="password">
+        <!-- 锁图标 -->
+        <span class="svg-container">
+          <el-icon>
+            <svg-icon icon="password"></svg-icon>
+          </el-icon>
+        </span>
+        <!-- on-input眼睛图标 -->
+        <el-input
+          placeholder="password"
+          name="password"
+          v-model="loginForm.password"
+        >
+          <span class="show-pwd">
+            <svg-icon icon="eye"></svg-icon>
+          </span>
+        </el-input>
+      </el-form-item>
+      <el-button type="primary" style="width: 100%; margin-bottom: 30px"
+        >登录</el-button
+      >
+    </el-form>
+  </div>
+</template>
+
+<script setup>
+import { reactive } from 'vue'
+
+const loginForm = reactive({
+  username: 'admin',
+  password: '123456'
+})
+</script>
+
+<style lang="scss" scoped>
+$bg: #2d3a4b;
+$dark_gray: #889aa4;
+$light_gray: #eee;
+$cursor: #fff;
+
+.login-container {
+  min-height: 100%;
+  width: 100%;
+  background-color: $bg;
+  overflow: hidden;
+  .login-form {
+    position: relative;
+    width: 520px;
+    max-width: 100%;
+    padding: 160px 35px 0;
+    margin: 0 auto;
+    overflow: hidden;
+    ::v-deep(.el-form-item) {
+      border: 1px solid rgba(255, 255, 255, 0.1);
+      background: rgba(0, 0, 0, 0.1);
+      border-radius: 5px;
+      color: #454545;
+    }
+    ::v-deep(.el-input) {
+      display: inline-block;
+      height: 47px;
+      width: 85%;
+      .el-input__wrapper {
+        background-color: transparent !important;
+        box-shadow: none;
+        vertical-align: middle;
+        height: 100%;
+        width: 100%;
+      }
+      .el-input__wrapper.is-focus {
+        box-shadow: none;
+      }
+      input {
+        background: transparent;
+        border: 0px;
+        -webkit-appearance: none;
+        border-radius: 0px;
+        padding: 12px 5px 12px 15px;
+        color: $light_gray;
+        height: 47px;
+        caret-color: $cursor;
+      }
+    }
+  }
+  .svg-container {
+    padding: 6px 5px 6px 15px;
+    color: $dark_gray;
+    vertical-align: middle;
+    display: inline-block;
+  }
+  .title-container {
+    position: relative;
+
+    .title {
+      font-size: 26px;
+      color: $light_gray;
+      margin: 0px auto 40px auto;
+      text-align: center;
+      font-weight: bold;
+    }
+    ::v-deep(.svg-language) {
+      position: absolute;
+      top: 4px;
+      right: 0;
+      background-color: #fff;
+      font-size: 22px;
+      padding: 4px;
+      border-radius: 4px;
+      cursor: pointer;
+    }
+  }
+  .show-pwd {
+    position: absolute;
+    right: 10px;
+    top: 7px;
+    font-size: 16px;
+    color: $dark_gray;
+    cursor: pointer;
+    user-select: none;
+  }
+}
+</style>
